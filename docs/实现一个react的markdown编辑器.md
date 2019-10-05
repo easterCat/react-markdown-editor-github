e4f74741c6af98c6f368c7381e58bdf44b8e407a
lilin <lilin@itcom888.com>
2019-10-05 17:50:48 +0800

    能够预览了

---
+++ docs/实现一个react的markdown编辑器.md
@@ -0,0 +1,15 @@
+## markdown 解析器
+
+- [markdown](https://www.npmjs.com/package/markdown)
+- [markdown-it](https://www.npmjs.com/package/markdown-it)
+- [marked](https://www.npmjs.com/package/marked)
+
+## 参考:
+
+- [震惊，看了这篇文章，前端菜鸟也能基于 react 打造属于自己的 markdown 在线编辑器！](https://github.com/accforgit/blog-data/blob/master/%E5%9C%A8%E7%BA%BF%E7%BC%96%E8%BE%91%E5%99%A8/README/README1.md)
+- [小 tip: 如何让 contenteditable 元素只能输入纯文本](https://www.zhangxinxu.com/wordpress/2016/01/contenteditable-plaintext-only/)
+- [react-markdown-editor-lite](https://github.com/HarryChen0506/react-markdown-editor-lite)
+- [手把手教你造一个基于 React 的 markdown 编辑器](https://juejin.im/post/5c73c8556fb9a049b222846d)
+- [react-markdown - Renders Markdown as pure React components.](https://github.com/rexxars/react-markdown)
+- [create-react-app](https://github.com/facebook/create-react-app)
+- [ant.design](https://ant.design/docs/react/introduce-cn)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MarkdownIt from "markdown-it";
+import emoji from "markdown-it-emoji";
+import subscript from "markdown-it-sub";
+import superscript from "markdown-it-sup";
+import footnote from "markdown-it-footnote";
+import deflist from "markdown-it-deflist";
+import abbreviation from "markdown-it-abbr";
+import insert from "markdown-it-ins";
+import mark from "markdown-it-mark";
+import tasklists from "markdown-it-task-lists";
+import hljs from "highlight.js/lib/highlight";
+import "highlight.js/styles/github.css";
+import "./styles/App.less";
+import content from "./content.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class HtmlRender extends React.Component {
+  render() {
+    return (
+      <div
+        dangerouslySetInnerHTML={{ __html: this.props.html }}
+        className={`custom-html-style ${this.props.className || ""}`}
+      />
+    );
+  }
 }
 
-export default App;
+export default class Md extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      previewContent: "",
+      content: content
+    };
+
+    this.mdParser = new MarkdownIt({
+      html: true,
+      linkify: true,
+      breaks: true,
+      highlight: (str, lang) => {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return '<pre class="hljs"><code>' + hljs.highlight(lang, str, true).value + "</code></pre>";
+          } catch (__) {}
+        }
+
+        return '<pre class="hljs"><code>' + this.mdParser.utils.escapeHtml(str) + "</code></pre>";
+      }
+    })
+      .use(emoji)
+      .use(subscript)
+      .use(superscript)
+      .use(footnote)
+      .use(deflist)
+      .use(abbreviation)
+      .use(insert)
+      .use(mark)
+      .use(tasklists, { enabled: this.taskLists });
+
+    this.leftScroll = React.createRef();
+    this.rightScroll = React.createRef();
+    this.onContentChange = this.onContentChange.bind(this);
+    this.renderToHtml = this.renderToHtml.bind(this);
+    this.initContent = this.initContent.bind(this);
+  }
+
+  componentDidMount() {
+    this.initContent();
+
+    let l = this.leftScroll.current;
+    let r = this.rightScroll.current;
+
+    l.addEventListener("mousewheel", function() {
+      r.scrollTop = l.scrollTop;
+    });
+    r.addEventListener("mousewheel", function() {
+      l.scrollTop = r.scrollTop;
+    });
+  }
+
+  initContent() {
+    const { content } = this.state;
+    const previewContent = this.renderToHtml(content);
+    console.log("previewContent :", previewContent);
+    this.setState({
+      previewContent
+    });
+  }
+
+  onContentChange(e) {
+    const content = e.target.value;
+    const previewContent = this.renderToHtml(content);
+
+    this.setState({
+      content,
+      previewContent
+    });
+  }
+
+  renderToHtml(text) {
+    return this.mdParser.render(text);
+  }
+
+  render() {
+    const { previewContent } = this.state;
+    return (
+      <div id="ReactMarkdown" style={{ height: "600px" }}>
+        <div className="rmd-header"></div>
+        <div className="rmd-tools"></div>
+        <div className="rmd-container">
+          <div ref={this.leftScroll} className="rmd-container-editor">
+            <textarea className="rmd-textarea" value={this.state.content} onChange={this.onContentChange} />
+          </div>
+          <div className="rmd-container-result" ref={this.rightScroll}>
+            <HtmlRender html={previewContent} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
